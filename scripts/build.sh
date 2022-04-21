a2ef4a1e49ef504d4f7c94d1b710dd6c725207d3
root <root@atlas-wn-601.roma1.infn.it>
2022-04-21 17:41:19 +0200

    Auto builder update

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 OSREL=${1}
 if [ -z "$OSREL" ] ; then
+  FULLPATH="`\ls $PWD/Dockerfile 2>/dev/null`"
+  if [ -z "$FULLPATH" ] ; then
+    echo "No Dockerfile found in local directory"
+    exit 1
+  fi
+  OSREL="`echo $FULLPATH | awk -F'/' '{print $(NF-2)$(NF-1)}'`"
+fi
+if [ -z "$OSREL" ] ; then
   echo "USAGE:"
   echo "`basename $0` <OSREL>"
   echo "e.g. `basename $0` centos7"
